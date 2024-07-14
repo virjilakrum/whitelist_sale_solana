2f22295d0b10f84ab840994c9877870806872189
Virjilakrum <asesenep15@gmail.com>
2024-07-14 13:51:25 +0300

    anchor init

---
+++ programs/whitelist_sale/src/lib.rs
@@ -0,0 +1,15 @@
+use anchor_lang::prelude::*;
+
+declare_id!("GwiGatU1wasA7wF7LtcrxjnZJD1AdwD4yE4FTBZN4k5o");
+
+#[program]
+pub mod whitelist_sale {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize {}
