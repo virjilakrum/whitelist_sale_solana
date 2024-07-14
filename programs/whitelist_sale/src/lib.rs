5df8764dcdfde9cce14b17aeeb89c00ca9f44509
Virjilakrum <asesenep15@gmail.com>
2024-07-14 14:02:50 +0300

    program context | users, limit, price

--- programs/whitelist_sale/src/lib.rs
+++ programs/whitelist_sale/src/lib.rs
@@ -7,10 +7,14 @@ declare_id!("GwiGatU1wasA7wF7LtcrxjnZJD1AdwD4yE4FTBZN4k5o");
 pub mod whitelist_sale {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize(ctx: Context<Initialize>, price: u64, limit: u64) -> Result<()> {
+        let sale_account = &mut ctx.accounts.sale_account;
+        sale_account.price = price;
+        sale_account.limit = limit;
+        sale_account.whitelisted_users = Vec::new();
+        sale_account.purchased = HashMap::new();
         Ok(())
     }
-}
 
 #[derive(Accounts)]
 pub struct Initialize {}
